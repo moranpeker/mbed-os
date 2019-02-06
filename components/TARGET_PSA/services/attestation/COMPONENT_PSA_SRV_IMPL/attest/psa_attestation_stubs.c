24282419be2db1a6ac5903fa91bdf970c258fc42
Moran Peker <moran.peker@arm.com>
2019-02-06 17:17:05 +0200

    attestation

---
+++ components/TARGET_PSA/services/attestation/COMPONENT_PSA_SRV_IMPL/attest/psa_attestation_stubs.c
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2018-2019, Arm Limited. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ */
+
+#include <stdint.h>
+#include "attestation.h"
+#include "attestation_key.h"
+
+enum psa_attest_err_t
+attest_get_and_register_initial_attestation_key(void)
+{
+    return PSA_ATTEST_ERR_SUCCESS;
+}
+
+enum psa_attest_err_t
+attest_check_memory_access(void *addr,
+                           uint32_t size,
+                           enum attest_memory_access access)
+{
+    return PSA_ATTEST_ERR_SUCCESS;
+}
---
+++ components/TARGET_PSA/services/attestation/COMPONENT_PSA_SRV_IMPL/attest/tfm_import/tfm_attest_hal.h
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2019, Arm Limited. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ */
+
+#ifndef __TFM_ATTEST_HAL_H__
+#define __TFM_ATTEST_HAL_H__
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * \brief Security lifecycle of the device
+ */
+enum tfm_security_lifecycle_t {
+    TFM_SLC_ASSEMBLY = 0,
+    TFM_SLC_PROVISONING,
+    TFM_SLC_PROVISIONED,
+    TFM_SLC_CONSTRAINED_DEBUG,
+    TFM_SLC_UNCONSTRAINED_DEBUG,
+};
+
+/**
+ * \brief Retrieve the security lifecycle of the device
+ *
+ * Security lifecycle is a mandatory claim in the initial attestation token.
+ *
+ * \return According to \ref tfm_security_lifecycle_t
+ */
+enum tfm_security_lifecycle_t tfm_attest_hal_get_security_lifecycle(void);
+
+/**
+ * \brief Retrieve the verification service indicator for initial attestation.
+ *
+ * It is used by relying party to locate a validation service for the token.
+ * It can be a text string that can be used to locate the service or can be a
+ * URL specifying the address of the service.
+ *
+ * \param[out]  size  Length of the string, without the termination zero byte.
+ *
+ * \return NULL pointer if not available otherwise the address of the
+ *         verification service string in the device memory.
+ */
+const char *
+tfm_attest_hal_get_verification_service(uint32_t *size);
+
+/**
+ * \brief Retrieve the name of the profile definition document for initial
+ *        attestation.
+ *
+ *  This document describes the 'profile' of the initial attestation token,
+ *  being a full description of the claims, their usage, verification and
+ *  token signing.
+ *
+ * \param[out]  size  Length of the document name, without the termination zero
+ *                    byte.
+ *
+ * \return NULL pointer if not available otherwise the address of the document
+ *         name string in the device memory.
+ */
+const char *
+tfm_attest_hal_get_profile_definition(uint32_t *size);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __TFM_ATTEST_HAL_H__ */
